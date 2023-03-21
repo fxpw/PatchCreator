8b5df424247e4a38a77a4f374622763bf65b72e2
fxpw <fxpw@outlook.com>
2023-03-21 11:38:17 +0300

    -rf

--- createPatch.sh
+++ createPatch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a comment!
 make deletempq
-cp /mnt/d/games/sirus/World\ of\ Warcraft\ Sirus/Data/ruRU/patch-ruRU-4.mpq /mnt/c/Users/fxpw/source/repos/fxpw/Falaffell\ Helper/originalMPQ
+cp -rf /mnt/d/games/sirus/World\ of\ Warcraft\ Sirus/Data/ruRU/patch-ruRU-4.mpq /mnt/c/Users/fxpw/source/repos/fxpw/Falaffell\ Helper/originalMPQ
 make
 ./build/apps/FH
-# cp  /mnt/c/Users/fxpw/source/repos/fxpw/Falaffell\ Helper/newMPQ/patch-ruRU-x.mpq /mnt/d/games/sirus/World\ of\ Warcraft\ Sirus/Data/ruRU/
+cp -rf /mnt/c/Users/fxpw/source/repos/fxpw/Falaffell\ Helper/newMPQ/patch-ruRU-x.mpq /mnt/d/games/sirus/World\ of\ Warcraft\ Sirus/Data/ruRU/
