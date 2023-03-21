59b786a3e7b7fa2cf18146a3faf2f8aa7e6f3e06
fxpw <fxpw@outlook.com>
2023-03-21 11:20:25 +0300

    вф

--- createPatch.sh
+++ createPatch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This is a comment!
-cp /mnt/d/games/sirus/World\ of\ Warcraft\ Sirus/Data/ruRU/patch-ruRU-4.mpq /mnt/c/Users/fxpw/source/repos/fxpw/Falaffell\ Helper/originalMPQ/
+make deletempq
+cp /mnt/d/games/sirus/World\ of\ Warcraft\ Sirus/Data/ruRU/patch-ruRU-4.mpq /mnt/c/Users/fxpw/source/repos/fxpw/Falaffell\ Helper/originalMPQ
 make
 ./build/apps/FH
