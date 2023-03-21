80c246357b9da474b86f6c6c48b8404eb50b905a
fxpw <fxpw@outlook.com>
2023-03-21 11:22:53 +0300

    копирование еще к себе файла

--- createPatch.sh
+++ createPatch.sh
@@ -4,3 +4,4 @@ make deletempq
 cp /mnt/d/games/sirus/World\ of\ Warcraft\ Sirus/Data/ruRU/patch-ruRU-4.mpq /mnt/c/Users/fxpw/source/repos/fxpw/Falaffell\ Helper/originalMPQ
 make
 ./build/apps/FH
+# cp  /mnt/c/Users/fxpw/source/repos/fxpw/Falaffell\ Helper/newMPQ/patch-ruRU-x.mpq /mnt/d/games/sirus/World\ of\ Warcraft\ Sirus/Data/ruRU/
