325a78850c5401d3e0b96f9f15dfd0dd950d841c
fxpw <fxpw@outlook.com>
2023-06-14 21:36:39 +0300

    change

--- PatchCreator/include/DBC.hpp
+++ PatchCreator/include/DBC.hpp
@@ -1,59 +1,59 @@
 #ifndef DBC_HPP
 #define DBC_HPP
 
-	#include <StormLib.h>
-	#include "structures/AllStructuresInclude.hpp"
+#include <StormLib.h>
+#include "structures/AllStructuresInclude.hpp"
 
-	template <typename DBCFileType, typename DBCRecordType>
-	class DBC {
-		public:
-			DBCFileType file;
-			std::string fileName;
-			std::map<std::string, stringref> mapForReUse;
-			// template <typename DBCFileType, typename DBCRecordType>
-			inline DBC(std::string dirWhereFindDBC, std::string dbcFileName){
-				fileName = dbcFileName;
-				std::ifstream dbcFile(dirWhereFindDBC + fileName, std::ios::binary);
-				if (!dbcFile.is_open())
-				{
-					//std::cerr << "Failed to open file: " << filename << std::endl;
-					return;
-				}
-				// Read header
-				dbcFile.read((char*)&file.header, sizeof(DBCHeader));
-				// std::cout<<"----------------------"<<std::endl;
-				// std::cout<<"filename:"<<dbcFileName<<std::endl;
-				// std::cout<<"head:"<<file.header.head<<std::endl;
-				// std::cout<<"recordCount:"<<file.header.recordCount<<std::endl;
-				// std::cout<<"fieldCount:"<<file.header.fieldCount<<std::endl;
-				// std::cout<<"recordSize:"<<file.header.recordSize<<std::endl;
-				// std::cout<<"stringBlockSize:"<<file.header.stringBlockSize<<std::endl;
-				file.records = new DBCRecordType[file.header.recordCount];
-				// Read records
-				for (uint32_t i = 0; i < file.header.recordCount; i++)
-				{
-					dbcFile.read(reinterpret_cast<char*>(&file.records[i]), sizeof(DBCRecordType));
-				}
-				// Allocate memory for string block
-				file.stringBlock = new char[file.header.stringBlockSize];
-				// Read string block
-				dbcFile.read(file.stringBlock, file.header.stringBlockSize);
-				dbcFile.close();
-			};
-			// template<typename DBCFileType, typename DBCRecordType>
-			inline bool CreateNewFileIn(std::string newDir){
-				std::ofstream outputFile(newDir + fileName, std::ios::binary);
-				outputFile.write(reinterpret_cast<const char*>(&file.header), sizeof(DBCHeader));
-				outputFile.write(reinterpret_cast<const char*>(file.records), file.header.recordCount * sizeof(DBCRecordType));
-				outputFile.write(file.stringBlock, file.header.stringBlockSize);
-				outputFile.close();
-				return true;
-			};
-			// template <typename DBCFileType, typename DBCRecordType>
-			inline ~DBC(){
-			};
-		private:
-	};
+template <typename DBCFileType, typename DBCRecordType>
+class DBC {
+	public:
+		DBCFileType file;
+		std::string fileName;
+		std::map<std::string, stringref> mapForReUse;
+		// template <typename DBCFileType, typename DBCRecordType>
+		inline DBC(std::string dirWhereFindDBC, std::string dbcFileName){
+			fileName = dbcFileName;
+			std::ifstream dbcFile(dirWhereFindDBC + fileName, std::ios::binary);
+			if (!dbcFile.is_open())
+			{
+				//std::cerr << "Failed to open file: " << filename << std::endl;
+				return;
+			}
+			// Read header
+			dbcFile.read((char*)&file.header, sizeof(DBCHeader));
+			// std::cout<<"----------------------"<<std::endl;
+			// std::cout<<"filename:"<<dbcFileName<<std::endl;
+			// std::cout<<"head:"<<file.header.head<<std::endl;
+			// std::cout<<"recordCount:"<<file.header.recordCount<<std::endl;
+			// std::cout<<"fieldCount:"<<file.header.fieldCount<<std::endl;
+			// std::cout<<"recordSize:"<<file.header.recordSize<<std::endl;
+			// std::cout<<"stringBlockSize:"<<file.header.stringBlockSize<<std::endl;
+			file.records = new DBCRecordType[file.header.recordCount];
+			// Read records
+			for (uint32_t i = 0; i < file.header.recordCount; i++)
+			{
+				dbcFile.read(reinterpret_cast<char*>(&file.records[i]), sizeof(DBCRecordType));
+			}
+			// Allocate memory for string block
+			file.stringBlock = new char[file.header.stringBlockSize];
+			// Read string block
+			dbcFile.read(file.stringBlock, file.header.stringBlockSize);
+			dbcFile.close();
+		};
+		// template<typename DBCFileType, typename DBCRecordType>
+		inline bool CreateNewFileIn(std::string newDir){
+			std::ofstream outputFile(newDir + fileName, std::ios::binary);
+			outputFile.write(reinterpret_cast<const char*>(&file.header), sizeof(DBCHeader));
+			outputFile.write(reinterpret_cast<const char*>(file.records), file.header.recordCount * sizeof(DBCRecordType));
+			outputFile.write(file.stringBlock, file.header.stringBlockSize);
+			outputFile.close();
+			return true;
+		};
+		// template <typename DBCFileType, typename DBCRecordType>
+		inline ~DBC(){
+		};
+	private:
+};
 
 #endif
 
--- PatchCreator/include/MPQ.hpp
+++ PatchCreator/include/MPQ.hpp
@@ -2,59 +2,59 @@
 #define MPQ_HPP
 
 #include "DBC.hpp"
-	class MPQ{
-		private:
-		static TCHAR* ConverterToTCHAR(const char* orig){
-			// TCHAR* tchar = 0;
-			std::string str = std::string(orig);
-			TCHAR* tchar = new TCHAR[str.size() + 1];
-			copy(str.begin(), str.end(), tchar);
-			tchar[str.size()] = 0;
-			return tchar;
+class MPQ{
+	private:
+	static TCHAR* ConverterToTCHAR(const char* orig){
+		// TCHAR* tchar = 0;
+		std::string str = std::string(orig);
+		TCHAR* tchar = new TCHAR[str.size() + 1];
+		copy(str.begin(), str.end(), tchar);
+		tchar[str.size()] = 0;
+		return tchar;
+	};
+	public:
+	static bool ExtractDBCFromMPQ(std::string pathToPatch){
+		HANDLE mpq;
+		bool isSuccess = SFileOpenArchive(ConverterToTCHAR(pathToPatch.c_str()), 0, STREAM_FLAG_WRITE_SHARE, &mpq);
+		if (!isSuccess) return false;
+		if (!mpq) {
+			return false;
 		};
-		public:
-		static bool ExtractDBCFromMPQ(std::string pathToPatch){
+		if (!SFileExtractFile(mpq, "DBFilesClient\\Spell.dbc", ConverterToTCHAR("./Spell.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
+		if (!SFileExtractFile(mpq, "DBFilesClient\\ItemDisplayInfo.dbc", ConverterToTCHAR("./ItemDisplayInfo.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
+		if (!SFileExtractFile(mpq, "DBFilesClient\\SpellItemEnchantment.dbc", ConverterToTCHAR("./SpellItemEnchantment.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
+		if (!SFileExtractFile(mpq, "DBFilesClient\\SpellVisualKit.dbc", ConverterToTCHAR("./SpellVisualKit.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
+		if (!SFileExtractFile(mpq, "DBFilesClient\\SpellVisualEffectName.dbc", ConverterToTCHAR("./SpellVisualEffectName.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
+		SFileCloseArchive(mpq);
+		return true;
+	};
+	static bool CreateMPQWhitDBC(std::string newPatchDirAndName = "error"){
+		try {
 			HANDLE mpq;
-			bool isSuccess = SFileOpenArchive(ConverterToTCHAR(pathToPatch.c_str()), 0, STREAM_FLAG_WRITE_SHARE, &mpq);
-			if (!isSuccess) return false;
-			if (!mpq) {
-				return false;
-			};
-			if (!SFileExtractFile(mpq, "DBFilesClient\\Spell.dbc", ConverterToTCHAR("./Spell.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
-			if (!SFileExtractFile(mpq, "DBFilesClient\\ItemDisplayInfo.dbc", ConverterToTCHAR("./ItemDisplayInfo.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
-			if (!SFileExtractFile(mpq, "DBFilesClient\\SpellItemEnchantment.dbc", ConverterToTCHAR("./SpellItemEnchantment.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
-			if (!SFileExtractFile(mpq, "DBFilesClient\\SpellVisualKit.dbc", ConverterToTCHAR("./SpellVisualKit.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
-			if (!SFileExtractFile(mpq, "DBFilesClient\\SpellVisualEffectName.dbc", ConverterToTCHAR("./SpellVisualEffectName.dbc"), SFILE_OPEN_FROM_MPQ)) return false;
-			SFileCloseArchive(mpq);
-			return true;
-		};
-		static bool CreateMPQWhitDBC(std::string newPatchDirAndName = "error"){
-			try {
-				HANDLE mpq;
-				remove(newPatchDirAndName.c_str());
-				bool isSucsess = SFileCreateArchive(ConverterToTCHAR(newPatchDirAndName.c_str()), MPQ_CREATE_ATTRIBUTES + MPQ_CREATE_ARCHIVE_V2, 0x000000010, &mpq);
-				if (!isSucsess){
-					remove("./Spell.dbc");
-					remove("./ItemDisplayInfo.dbc");
-					remove("./SpellItemEnchantment.dbc");
-					remove("./SpellVisualKit.dbc");
-					remove("./SpellVisualEffectName.dbc");
-					return false;
-				};
-				SFileAddFileEx(mpq, ConverterToTCHAR("./Spell.dbc"), "DBFilesClient\\Spell.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
+			remove(newPatchDirAndName.c_str());
+			bool isSucsess = SFileCreateArchive(ConverterToTCHAR(newPatchDirAndName.c_str()), MPQ_CREATE_ATTRIBUTES + MPQ_CREATE_ARCHIVE_V2, 0x000000010, &mpq);
+			if (!isSucsess){
 				remove("./Spell.dbc");
-				SFileAddFileEx(mpq, ConverterToTCHAR("./ItemDisplayInfo.dbc"), "DBFilesClient\\ItemDisplayInfo.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
 				remove("./ItemDisplayInfo.dbc");
-				SFileAddFileEx(mpq, ConverterToTCHAR("./SpellItemEnchantment.dbc"), "DBFilesClient\\SpellItemEnchantment.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
 				remove("./SpellItemEnchantment.dbc");
-				SFileAddFileEx(mpq, ConverterToTCHAR("./SpellVisualKit.dbc"), "DBFilesClient\\SpellVisualKit.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
 				remove("./SpellVisualKit.dbc");
-				SFileAddFileEx(mpq, ConverterToTCHAR("./SpellVisualEffectName.dbc"), "DBFilesClient\\SpellVisualEffectName.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
 				remove("./SpellVisualEffectName.dbc");
-				return SFileCloseArchive(mpq);
-			}catch (...){
 				return false;
 			};
+			SFileAddFileEx(mpq, ConverterToTCHAR("./Spell.dbc"), "DBFilesClient\\Spell.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
+			remove("./Spell.dbc");
+			SFileAddFileEx(mpq, ConverterToTCHAR("./ItemDisplayInfo.dbc"), "DBFilesClient\\ItemDisplayInfo.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
+			remove("./ItemDisplayInfo.dbc");
+			SFileAddFileEx(mpq, ConverterToTCHAR("./SpellItemEnchantment.dbc"), "DBFilesClient\\SpellItemEnchantment.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
+			remove("./SpellItemEnchantment.dbc");
+			SFileAddFileEx(mpq, ConverterToTCHAR("./SpellVisualKit.dbc"), "DBFilesClient\\SpellVisualKit.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
+			remove("./SpellVisualKit.dbc");
+			SFileAddFileEx(mpq, ConverterToTCHAR("./SpellVisualEffectName.dbc"), "DBFilesClient\\SpellVisualEffectName.dbc", MPQ_FILE_COMPRESS + MPQ_FILE_REPLACEEXISTING, MPQ_COMPRESSION_ZLIB, MPQ_COMPRESSION_NEXT_SAME);
+			remove("./SpellVisualEffectName.dbc");
+			return SFileCloseArchive(mpq);
+		}catch (...){
+			return false;
 		};
 	};
+};
 #endif
